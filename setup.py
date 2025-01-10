25ba4870a4a815b8ae0c3b7601b4577067e7abfc
Jorge Gidi <jorgegidi@gmail.com>
2025-01-10 12:58:04 -0300

    update package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="ECC2025",
-    version="0.1.5",
+    version="0.1.6",
     description="ECC2025",
     url="https://github.com/MIROptics/ECC2025",
     author="Luciano Pereira",
